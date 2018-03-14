ec8bed611bb28ec9c35bba9c9df2d8d78c29dfbc
BuildTools <so.thatsit@hotmail.com>
2018-03-15 00:44:41 +0800

    more documentation

--- src/main/java/com/sothatsit/cits2200/algorithm/Sorter.java
+++ src/main/java/com/sothatsit/cits2200/algorithm/Sorter.java
@@ -4,6 +4,7 @@ import CITS2200.Sort;
 
 /**
  * Allow using an insertion sort, quick sort or merge sort to sort an array of longs.
+ * Counts the number of assignments done by each sorting algorithm in sorting an array.
  *
  * @author Paddy Lamont
  */
@@ -11,10 +12,16 @@ public class Sorter implements Sort {
 
     private int assignmentCount;
 
+    /**
+     * @return the number of array assignments that happened since the last {@link #reset()}.
+     */
     public int getCount() {
         return assignmentCount;
     }
 
+    /**
+     * Reset the count of array assignments to zero.
+     */
     public void reset() {
         assignmentCount = 0;
     }
@@ -23,6 +30,11 @@ public class Sorter implements Sort {
     // INSERTION SORT
     //
 
+    /**
+     * Sort {@param array} using an insertion sort.
+     *
+     * @param array the array to be sorted
+     */
     public void insertionSort(long[] array) {
         for(int j = 1; j < array.length; ++j) {
             long key = array[j];
@@ -44,6 +56,11 @@ public class Sorter implements Sort {
     // QUICK SORT
     //
 
+    /**
+     * Sort {@param array} using a quick sort.
+     *
+     * @param array the array to be sorted
+     */
     public void quickSort(long[] array) {
         quickSort(array, 0, array.length - 1);
     }
@@ -57,6 +74,15 @@ public class Sorter implements Sort {
         }
     }
 
+    /**
+     * Select and sort a pivot into its correct position in array between start and end inclusive.
+     *
+     * @param array the array to partition
+     * @param start the start of the partition, inclusive
+     * @param end   the end of the partition, inclusive
+     *
+     * @return the index of the new location of the pivot
+     */
     private int partition(long[] array, int start, int end) {
         long pivot = array[end];
         int pivotIndex = start - 1;
@@ -73,6 +99,13 @@ public class Sorter implements Sort {
         return pivotIndex;
     }
 
+    /**
+     * Swap the elements at {@param index1} and {@param index2} of {@param array}.
+     *
+     * @param array  the array in which the two elements will be swapped
+     * @param index1 the index of the first element to be swapped
+     * @param index2 the index of the second element to be swapped
+     */
     private void swap(long[] array, int index1, int index2) {
         long temp = array[index1];
         array[index1] = array[index2];
@@ -83,11 +116,30 @@ public class Sorter implements Sort {
     //
     // MERGE SORT
     //
+    // Author: Tim French
+    //
 
+    /**
+     * Executes the merge sort algorithm sorting the argument array.
+     * There is no return as the parameter is to be mutated.
+     *
+     * @param a the array of long integers to be sorted
+     **/
     public void mergeSort(long[] a) {
         mergeSort(a, 0, a.length-1);
     }
 
+    /**
+     * A private method to merge the elements in the array between p and r.
+     * the array a, between the indices p and r, inclusive.
+     * Given the array is sorted between p and q and q+1 and r
+     * sorts the array between p and r.
+     *
+     * @param a the array to be sorted, which is mutated by the method
+     * @param p the lower index of the range to be partitioned
+     * @param q the midpoint of the two sorted sections.
+     * @param r the upper index of the range to be paritioned
+     **/
     private void merge(long[] a, int p, int q, int r) {
         int n = q-p+1;
         int m = r-q;
@@ -111,6 +163,10 @@ public class Sorter implements Sort {
         }
     }
 
+    /**
+     * Overloads the mergeSort method with parameters to set the
+     * range to be sorted.
+     **/
     private void mergeSort(long[] a, int p, int r) {
         if(p < r) {
             int i = (p+r)/2;
--- src/main/java/com/sothatsit/cits2200/util/Checks.java
+++ src/main/java/com/sothatsit/cits2200/util/Checks.java
@@ -2,26 +2,56 @@ package com.sothatsit.cits2200.util;
 
 import java.util.Objects;
 
+/**
+ * A collection of assertions to guarantee parameter constraints or to guarantee conditions in tests.
+ *
+ * @author Paddy Lamont
+ */
 public class Checks {
 
-    public static class AssertFailure extends RuntimeException {
-        public AssertFailure() {
+    /**
+     * An exception thrown when an assertion fails.
+     */
+    public static class AssertionFailure extends RuntimeException {
+
+        /**
+         * Construct an assertion failure exception without a message.
+         */
+        public AssertionFailure() {
             super();
         }
 
-        public AssertFailure(String message) {
-            super(message);
+        /**
+         * Construct an assertion failure with the reason {@param reason}.
+         *
+         * @param reason the reason for this assertion failure
+         */
+        public AssertionFailure(String reason) {
+            super(reason);
         }
     }
 
+    /**
+     * Throw an empty assertion failure.
+     */
     public static void fail() {
-        throw new AssertFailure();
+        throw new AssertionFailure();
     }
 
-    public static void fail(String message) {
-        throw new AssertFailure(message);
+    /**
+     * Throw an assertion failure with the reason {@param reason};
+     *
+     * @param reason the reason for this assertion failure
+     */
+    public static void fail(String reason) {
+        throw new AssertionFailure(reason);
     }
 
+    /**
+     * Assert that {@param value} is true.
+     *
+     * @param value the value that you assert to be true
+     */
     public static void assertTrue(boolean value) {
         if(value)
             return;
@@ -29,21 +59,46 @@ public class Checks {
         fail();
     }
 
-    public static void assertTrue(boolean value, String message) {
+    /**
+     * Assert that {@param value} is true, throwing an assertion failure
+     * with the reason {@param reason} if the assertion fails.
+     *
+     * @param value  the value that you assert to be true
+     * @param reason the reason if this assertion fails
+     */
+    public static void assertTrue(boolean value, String reason) {
         if(value)
             return;
 
-        fail(message);
+        fail(reason);
     }
 
+    /**
+     * Assert that {@param value1} is equal to {@param value2}.
+     *
+     * @param value1 the first value
+     * @param value2 the second value
+     */
     public static void assertEquals(Object value1, Object value2) {
         assertTrue(Objects.equals(value1, value2), value1 + " does not equal " + value2);
     }
 
+    /**
+     * Assert that {@param value} is non-null.
+     *
+     * @param value the value that you assert to not be null
+     * @param name  the name of the value that must be non-null
+     */
     public static void assertNonNull(Object value, String name) {
         assertTrue(value != null, name + " must be non-null");
     }
 
+    /**
+     * Assert that running {@param runnable} throws an exception of type {@param exceptionClass}.
+     *
+     * @param runnable       the runnable that you assert will throw the exception
+     * @param exceptionClass the or a super class of the exception you assert will be thrown
+     */
     public static void assertThrows(Runnable runnable, Class<? extends Exception> exceptionClass) {
         String exceptionName = exceptionClass.getSimpleName();
 
